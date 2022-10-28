8463ce00e6b57092932bd12a3d00654bf9e429b7
fanggadewangga <fanggadewangga@gmail.com>
2022-10-28 07:38:05 +0700

    add: landing screen and some resources

---
+++ app/src/main/java/com/raion/hunter/landing/LandingActivity.kt
@@ -0,0 +1,12 @@
+package com.raion.hunter.landing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.raion.hunter.R
+
+class LandingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+}
