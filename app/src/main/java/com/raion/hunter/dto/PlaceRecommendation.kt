59c7c901d153fc44164ba7dfc6e0df82fa3a1850
ibrahimss02 <tosaritonosambataheri@gmail.com>
2022-10-28 17:57:59 +0700

    add 4 main fragments

--- app/src/main/java/com/raion/hunter/dto/PlaceRecommendation.kt
+++ app/src/main/java/com/raion/hunter/dto/PlaceRecommendation.kt
@@ -1,8 +1,24 @@
 package com.raion.hunter.dto
 
+import android.content.Context
 import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.raion.hunter.R
 
 data class PlaceRecommendation(
     val name: String,
     val image: Drawable
 )
+
+object DummyPlace {
+    fun getData(context: Context): ArrayList<PlaceRecommendation> {
+        return arrayListOf(
+            PlaceRecommendation("Kampung Warna Warni", ContextCompat.getDrawable(context, R.drawable.iv_kampung_warna)!!),
+            PlaceRecommendation("Kampung Warna Warni", ContextCompat.getDrawable(context, R.drawable.iv_kampung_warna)!!),
+            PlaceRecommendation("Kampung Warna Warni", ContextCompat.getDrawable(context, R.drawable.iv_kampung_warna)!!),
+            PlaceRecommendation("Kampung Warna Warni", ContextCompat.getDrawable(context, R.drawable.iv_kampung_warna)!!),
+            PlaceRecommendation("Kampung Warna Warni", ContextCompat.getDrawable(context, R.drawable.iv_kampung_warna)!!),
+            PlaceRecommendation("Kampung Warna Warni", ContextCompat.getDrawable(context, R.drawable.iv_kampung_warna)!!),
+        )
+    }
+}
--- app/src/main/java/com/raion/hunter/homepage/FragmentHomepage.kt
+++ app/src/main/java/com/raion/hunter/homepage/FragmentHomepage.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.raion.hunter.R
 import com.raion.hunter.databinding.FragmentHomepageBinding
+import com.raion.hunter.dto.DummyPlace
 
 class FragmentHomepage : Fragment() {
 
@@ -19,9 +20,9 @@ class FragmentHomepage : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentHomepageBinding.inflate(layoutInflater)
 
-
-
-
+        val adapter = PlaceRecommendationAdapter()
+        adapter.submitList(DummyPlace.getData(requireContext()))
+        binding.placeRecommendationRv.adapter = adapter
 
 
         return binding.root
