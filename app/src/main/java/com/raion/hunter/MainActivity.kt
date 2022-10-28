529bd5b020fe9dca5b9a9eb46d21f7d320081ae1
ibrahimss02 <tosaritonosambataheri@gmail.com>
2022-10-28 16:45:06 +0700

    add fragment_homepage

--- app/src/main/java/com/raion/hunter/MainActivity.kt
+++ app/src/main/java/com/raion/hunter/MainActivity.kt
@@ -2,10 +2,23 @@ package com.raion.hunter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.raion.hunter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentManager = supportFragmentManager.findFragmentById(R.id.frame_layout) as NavHostFragment
+        val navController = fragmentManager.navController
+
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
