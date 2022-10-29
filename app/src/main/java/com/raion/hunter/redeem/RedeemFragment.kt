087e46540da6cfe7242584058cfc835177a6f274
fanggadewangga <fanggadewangga@gmail.com>
2022-10-29 23:10:21 +0700

    add: bottomsheet layout

--- app/src/main/java/com/raion/hunter/redeem/RedeemFragment.kt
+++ app/src/main/java/com/raion/hunter/redeem/RedeemFragment.kt
@@ -5,15 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.raion.hunter.R
+import com.raion.hunter.databinding.FragmentRedeemBinding
 
 class RedeemFragment : Fragment() {
+
+    private lateinit var binding: FragmentRedeemBinding
+    private lateinit var bottomSheet: BottomSheetBehavior<ConstraintLayout>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_redeem, container, false)
+        binding = FragmentRedeemBinding.inflate(layoutInflater)
+        bottomSheet = BottomSheetBehavior.from(binding.bottomSheetRedeem).apply {
+            expandedOffset = 120
+            peekHeight = 640
+            isFitToContents = false
+
+            addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+
+                    }
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                    TODO("Not yet implemented")
+                }
+            }
+            )
+        }
+
+        return binding.root
     }
 }
