6d85d250afe01d13d7e07f83a59f8edd79f6fc72
fanggadewangga <fanggadewangga@gmail.com>
2022-10-29 13:49:08 +0700

    add: leaderboard fragment

---
+++ app/src/main/java/com/raion/hunter/leaderboard/LeaderboardFragment.kt
@@ -0,0 +1,22 @@
+package com.raion.hunter.leaderboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.raion.hunter.R
+
+
+class LeaderboardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_leaderboard, container, false)
+    }
+
+
+}
